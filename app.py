f7818560a7de67fdc918c93969866e6a9eba1187
Asmaa Ali <asmaa.a.abdelwahab@gmail.com>
2025-04-22 08:54:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ logger = logging.getLogger(__name__)
 
 # Email configuration (set these in your environment variables)
 EMAIL_ADDRESS = "asmaa@edelweissconnect.com"
-EMAIL_PASSWORD = "kqyh cdzz pvgo iqlx"
+EMAIL_PASSWORD = "mttv jxju ppud itqo"
 
 # Streamlit App Setup
 st.set_page_config(page_title="PubMed ChemInsight", page_icon="⚗️")
